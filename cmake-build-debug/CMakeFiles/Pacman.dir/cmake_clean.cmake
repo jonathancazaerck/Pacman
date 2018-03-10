5d5ebcbb08e1551222cdfece11a63a1b7b96d2c0
Jonathan Cazaerck <jonathan.cazaerck@gmail.com>
2018-03-10 11:49:15 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/Pacman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pacman.dir/main.cpp.o"
+  "Pacman.pdb"
+  "Pacman"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
